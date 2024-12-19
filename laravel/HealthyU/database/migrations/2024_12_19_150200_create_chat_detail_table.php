5b8c0e06fbbba4e86674ceb82e9f37b388c9711a
michaelkurniawan <mchlkk98@gmail.com>
2024-12-19 22:38:52 +0700

    making database, done but not sure need recheck

---
+++ laravel/HealthyU/database/migrations/2024_12_19_150200_create_chat_detail_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chat_detail', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('chat_room_id');
+            $table->text('message');
+            $table->timestamps();
+
+            // $table->foreign('challenge_id')->references('id')->on('challenges')->onDelete('cascade');
+            $table->foreign('chat_room_id')->references('id')->on('chat_room')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chat_detail');
+    }
+};
