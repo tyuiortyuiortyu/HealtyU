1f9adb2c5a1e2b9aaafc2bc283221e0ac999d5df
Bodhi <166287178+HZX878@users.noreply.github.com>
2025-03-10 21:50:28 +0700

    Terbaru, modified by meki

--- HealthyU/routes/api.php
+++ HealthyU/routes/api.php
@@ -22,7 +22,7 @@ use App\Http\Controllers\api\community\CommunityController;
 route::prefix('auth')->group(function(){
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
-    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware(JwtMiddleware::class);
     Route::get('/getUserData', [AuthController::class, 'getUserData'])->middleware(JwtMiddleware::class);
 });
 
